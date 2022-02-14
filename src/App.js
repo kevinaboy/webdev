51648ac903401a426b0be2d4491ba6b7485e2abe
School <kaboy@icloud.com>
2022-02-14 01:59:14 -0600

    moved the remaining Portfolio section from App.js to Portfolio.js

--- src/App.js
+++ src/App.js
@@ -152,67 +152,6 @@ function App() {
         </section>
         <hr className="m-0" />
 
-        <section className="resume-section" id="portfolio">
-          <div className="resume-section-content">
-            <h2 className="mb-5">Portfolio</h2>
-            {/*
-      <!-- <p>Coming soon...</p> --> */}
-            <div className="container">
-              <div className="row">
-                <div className="col-sm-12 col-md-4">
-                  <div className="card">
-                    <img className="card-img-top" src="assets/images/want-to-buy-hp.png" alt="Want To Buy Homepage Screenshot" />
-                    <div className="card-body">
-                      <h5 className="card-title">Want To Buy</h5>
-                      {/*
-                <!-- <h6 className="card-subtitle mb-2 text-muted">HTML, CSS, JAVASCRIPT, MAPBOX API</h6> --> */}
-                      <p className="card-text">A twist to the typical e-commerce website, this web app uses Node.js and
-                        Express.js to
-                        create a RESTful API, Handlebars.js as the templating engine, MySQL and Sequelize ORM for the
-                        database, Amazon Web Services S3 for storing images, and deployed using Heroku.
-                      </p>
-                      <a href="https://want-to-buy.herokuapp.com/" className="card-link">View website</a>
-                      <br />
-                      <a href="https://github.com/taylorkeltgen/want-to-buy/" className="card-link">View repo</a>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-sm-12 col-md-4">
-                  <div className="card">
-                    <img className="card-img-top" src="assets/images/tacoshop_hp_med.png" alt="Top 3 Taco Shops Homepage Screenshot" />
-                    <div className="card-body">
-                      <h5 className="card-title">Top 3 Taco Shops</h5>
-                      {/*
-                <!-- <h6 className="card-subtitle mb-2 text-muted">HTML, CSS, JAVASCRIPT, MAPBOX API</h6> --> */}
-                      <p className="card-text">Any day is a good day for a taco. This highly collaborative project utilizes
-                        JavaScript, Mapbox GL JS, OpenWeather One Call API, and Materialize, a modern responsive CSS
-                        framework based on Material Design by Google.
-                      </p>
-                      <a href="https://trujilml.github.io/top-3-taco-shops" className="card-link">View website</a>
-                      <br />
-                      <a href="https://github.com/trujilml/top-3-taco-shops" className="card-link">View repo</a>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-sm-12 col-md-4">
-                  <div className="card">
-                    <img className="card-img-top" src="assets/images/horiseon_hp_med.png" alt="Card image cap" />
-                    <div className="card-body">
-                      <h5 className="card-title">Code Refactor</h5>
-                      <p className="card-text">Reworked the CSS to make it more efficient by consolidating CSS selectors and
-                        properties, organizing them to follow the semantic structure of the HTML elements, and including
-                        comments before each element or section of the page.</p>
-                      <a href="https://kevinaboy.github.io/code-refactor/" className="card-link">View website</a>
-                      <br />
-                      <a href="https://github.com/kevinaboy/code-refactor" className="card-link">View repo</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
         <Portfolio />
 
       </div >
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,6 +9,64 @@ function Portfolio(props) {
     <>
       <section className="resume-section" id="portfolio">
         <div className="resume-section-content">
+          <h2 className="mb-5">Portfolio</h2>
+
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12 col-md-4">
+                <div className="card">
+                  <img className="card-img-top" src="assets/images/want-to-buy-hp.png" alt="Want To Buy Homepage Screenshot" />
+                  <div className="card-body">
+                    <h5 className="card-title">Want To Buy</h5>
+
+                    <p className="card-text">A twist to the typical e-commerce website, this web app uses Node.js and
+                      Express.js to
+                      create a RESTful API, Handlebars.js as the templating engine, MySQL and Sequelize ORM for the
+                      database, Amazon Web Services S3 for storing images, and deployed using Heroku.
+                    </p>
+                    <a href="https://want-to-buy.herokuapp.com/" className="card-link">View website</a>
+                    <br />
+                    <a href="https://github.com/taylorkeltgen/want-to-buy/" className="card-link">View repo</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-12 col-md-4">
+                <div className="card">
+                  <img className="card-img-top" src="assets/images/tacoshop_hp_med.png" alt="Top 3 Taco Shops Homepage Screenshot" />
+                  <div className="card-body">
+                    <h5 className="card-title">Top 3 Taco Shops</h5>
+
+                    <p className="card-text">Any day is a good day for a taco. This highly collaborative project utilizes
+                      JavaScript, Mapbox GL JS, OpenWeather One Call API, and Materialize, a modern responsive CSS
+                      framework based on Material Design by Google.
+                    </p>
+                    <a href="https://trujilml.github.io/top-3-taco-shops" className="card-link">View website</a>
+                    <br />
+                    <a href="https://github.com/trujilml/top-3-taco-shops" className="card-link">View repo</a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-sm-12 col-md-4">
+                <div className="card">
+                  <img className="card-img-top" src="assets/images/horiseon_hp_med.png" alt="Card image cap" />
+                  <div className="card-body">
+                    <h5 className="card-title">Code Refactor</h5>
+                    <p className="card-text">Reworked the CSS to make it more efficient by consolidating CSS selectors and
+                      properties, organizing them to follow the semantic structure of the HTML elements, and including
+                      comments before each element or section of the page.</p>
+                    <a href="https://kevinaboy.github.io/code-refactor/" className="card-link">View website</a>
+                    <br />
+                    <a href="https://github.com/kevinaboy/code-refactor" className="card-link">View repo</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="resume-section" id="portfolio">
+        <div className="resume-section-content">
           {/*
       <!-- <h2 className="mb-5">Portfolio</h2> -->
       <!-- <p>Coming soon...</p> --> */}
